e1d1b9ae9218b28b6d6908434b667eead4f3ee60
B49 <amannraj6349@gmail.com>
2026-06-28 20:56:33 +0530

    Create DAY 03.py

---
+++ DAY 03.py
@@ -0,0 +1,29 @@
+#writing our first pythn program
+print("hello world")
+
+ # why we use comment to pass code to other then that person understand what you use here 
+# (#) is use for single line comment 
+# (''' or """) is use for multiple line comment
+
+# adding space at the beginning of a line shows code error
+'''
+what is variable 
+A variable is a container that stores data in the computeer's memory 
+it has a name and we can use that name to access data anythime
+
+types of data types
+int ---> interger
+float---> deciman numners 
+str---> text
+bool--->true or false
+list --->order ,mutual items
+tuple --->order, immutable items
+dict--->key value pairs
+'''
+
+'''
+data conversion
+'''
+
+print(int(2.3))
+# as we can see 2.3 is float data type but with help of int() we convert into whole number
